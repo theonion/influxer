e1009624256acee3acad7f9718670f6cd4732e88
vforgione <the.vincedent@icloud.com>
2014-12-12 16:39:43 -0600

    fuck being fancy

--- influxer/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = '0.0.1'
-__authors__ = [
-    {'name': 'Vince Forgione', 'email': 'vforgione@theonion.com'},
-]
--- setup.py
+++ setup.py
@@ -1,32 +1,15 @@
-import codecs
 from distutils.core import setup
-import os
-
-from influxer import __version__ as version, __authors__ as authors
-
-
-def get_authors():
-    return ', '.join(author['name'] for author in authors)
-
-
-def get_author_emails():
-    return ', '.join(author['email'] for author in authors)
-
-
-def get_dependencies():
-    filename = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-    with codecs.open(filename, 'r', encoding='utf8') as fh:
-        dependencies = [line.strip() for line in fh]
-    return dependencies
 
 
 setup(
     name='influxer',
-    version=version,
+    version='0.0.1',
     description='A small pixel tracker built in python and designed to send data to InfluxDB',
-    author=get_authors(),
-    author_email=get_author_emails(),
+    author='Vince Forgione',
+    author_email='vforgione@theonion.com',
     packages=['influxer'],
-    install_requires=get_dependencies(),
+    install_requires=[
+        'influxdb==0.1.13'
+    ],
     url='https://github.com/theonion/influxer'
 )
