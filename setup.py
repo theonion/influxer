8b9b91128778eb29e63a7f8044c64a3c374a06fa
vforgione <v.forgione@gmail.com>
2015-04-20 13:21:04 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='influxer',
-    version='0.0.4',
+    version='0.0.5',
     description='A small pixel tracker built in python and designed to send data to InfluxDB and replace TinyTracker',
     author='Vince Forgione',
     author_email='vforgione@theonion.com',
