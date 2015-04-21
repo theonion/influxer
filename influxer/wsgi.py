dc58bca6c178b03b3308bc081c457cce2452b4d1
vforgione <v.forgione@gmail.com>
2015-04-20 21:34:01 -0500

    massive rewrite

--- influxer/wsgi.py
+++ influxer/wsgi.py
@@ -2,120 +2,124 @@
 # -*- coding: utf-8 -*-
 
 import base64
+from collections import Counter
 import logging
-import logging.handlers
-import re
+
 try:
     from urllib.parse import parse_qs
 except ImportError:
     from urlparse import parse_qs
 import os
 
+import gevent
+from gevent import queue
 from influxdb.client import InfluxDBClient
 
 
+# set up influxdb client
+host = os.environ.get("INFLUXER_INFLUXDB_HOST_IP", "localhost")
+port = os.environ.get("INFLUXER_INFLUXDB_PORT", 8086)
+user = os.environ.get("INFLUXER_INFLUXDB_USERNAME", "root")
+pwd = os.environ.get("INFLUXER_INFLUXDB_PASSWORD", "root")
+db = os.environ.get("INFLUXER_INFLUXDB_DB", "influxdb")
+client = InfluxDBClient(host, port, user, pwd, db)
+
+# build the response gif
+gif = base64.b64decode("R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
+
+# init the gevent queue
+events_queue = queue.Queue()
+flush_interval = os.environ.get("INFLUXER_FLUSH_INTERVAL", 60)  # seconds
+
+# init the logger
 logger = logging.getLogger('influxer')
 logger.setLevel(logging.INFO)
-log_file_name = os.environ.get('LOG_FILE_NAME', 'influxer.log')
+log_file_name = os.environ.get('INFLUXER_LOG_FILE_NAME', 'influxer.log')
 handler = logging.handlers.RotatingFileHandler(log_file_name, maxBytes=5000000, backupCount=5)
 logger.addHandler(handler)
 
-gif = base64.b64decode('R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
 
-regex = re.compile(r'^.*\.\d+$')
-
-host = os.environ.get('INFLUXDB_HOST_IP', 'localhost')
-port = os.environ.get('INFLUXDB_PORT', 8086)
-user = os.environ.get('INFLUXDB_USERNAME', 'root')
-pwd = os.environ.get('INFLUXDB_PASSWORD', 'root')
-db = os.environ.get('INFLUXDB_DB', 'influxdb')
-client = InfluxDBClient(host, port, user, pwd, db)
-logger.info('connected to influxdb: {}:{}'.format(host, port))
-
-
-def influxer_1(qs):
-    """drop in replacement for tinytracker style event monitoring. this strips important information from the
-    query string and pushes it to influxdb for storage.
-
-    :param qs: the parsed query string
+def send_data(events, additional):
+    """creates data point payloads and sends them to influxdb
     """
-    try:
-        events = qs.get('event', [])
-        for event in events:
-            if re.match(regex, event):
-                event_data = event.split('.')
-                property = event_data[0]
-                content_id = event_data[-1]
-                body = [{
-                    'name': property,
-                    'columns': ['content_id', 'clicks'],
-                    'points': [[content_id, 1]]
-                }]
-                res = client.write_points(body)
-                logger.info('{} {}'.format(res, body))
-    except Exception as e:
-        logger.error(str(e))
+    bodies = {}
+    for (site, content_id), count in events.items():
+        # influxdb will take an array of arrays of values, cutting down on the number of requests
+        # needed to be sent to it wo write data
+        bodies.setdefault(site, [])
+
+        # get additional info
+        event, path = additional.get((site, content_id), (None, None))
+
+        # append the point
+        bodies[site].append([content_id, event, path])
+
+    for site, points in bodies.items():
+        # send payload to influxdb
+        try:
+            client.write_points([{
+                "name": site,
+                "columns": ["content_id", "event", "path"],
+                "points": points,
+            }])
+        except Exception as e:
+            logger.error(str(e))
 
 
-def influxer_2(qs):
-    """better replacement for tinytracker event monitoring. this uses the dotted tagging style available for
-    influxdb v0.8.7+.
-
-    :param qs: the parsed query string
+def count_events():
+    """pulls data from the queue, tabulates it and spawns a send event
     """
-    try:
-        events = qs.get('event', [])
-        for event in events:
-            if re.match(regex, event):
-                event_data = event.split('.')
-                content_id = event_data[-1]
-                name = '.'.join(event_data[:-1])
-                body = [{
-                    'name': name,
-                    'columns': ['content_id', 'clicks'],
-                    'points': [[content_id, 1]],
-                }]
-                res = client.write_points(body)
-                logger.info('{}, {}'.format(res, body))
-    except Exception as e:
-        logger.error(str(e))
+    while 1:
+        # sleep and let the queue build up
+        gevent.sleep(flush_interval)
+
+        # init the data points containers
+        events = Counter()
+        additional = {}
+
+        # flush the queue
+        while 1:
+            try:
+                site, content_id, event, path = events_queue.get_nowait()
+                events[(site, content_id)] += 1
+                if event and path:
+                    additional[(site, content_id)] = (event, path)
+            except queue.Empty:
+                break
+            except Exception as e:
+                logger.error(str(e))
+                break
+
+        # after tabulating, spawn a new thread to send the data to influxdb
+        if len(events):
+            gevent.spawn(send_data, events, additional)
 
 
-def influxer_3(params):
-    """parses qs for `site`, `content_id`, `event`, `extra` for more verbose and versatile tracking
-    """
-    site = params.get('site')
-    content_id = params.get('content_id')
-    event = params.get('event')
-    extra = params.get('extra')
-    body = [{
-        'name': site,
-        'columns': ['content_id', 'event', 'extra', 'clicks'],
-        'points': [content_id, event, extra, 1]
-    }]
-    try:
-        res = client.write_points(body)
-        logger.info('{}, {}'.format(res, body))
-    except Exception as e:
-        logger.error(str(e))
+# create the wait loop
+gevent.spawn(count_events)
 
 
 def application(env, start_response):
-    if env['PATH_INFO'] == '/influx.gif':
-        start_response('200 OK', [('Content-Typ', 'image/gif')])
+    """wsgi application
+    """
+    path = env["PATH_INFO"]
+
+    if path == "/influx.gif":
+        # send response
+        start_response("200 OK", [("Content-Type", "image/gif")])
         yield gif
-        qs = parse_qs(env['QUERY_STRING'])
-        influxer_1(qs)
-    elif env['PATH_INFO'] == '/influxer2.gif':
-        start_response('200 OK', [('Content-Typ', 'image/gif')])
-        yield gif
-        qs = parse_qs(env['QUERY_STRING'])
-        influxer_2(qs)
-    elif env['PATH_INFO'] == '/influxer3.gif':
-        start_response('200 OK', [('Content-Typ', 'image/gif')])
-        yield gif
-        qs = parse_qs(env['QUERY_STRING'])
-        influxer_3(qs)
+
+        # parse the query params and stick them in the queue
+        params = parse_qs(env["QUERY_STRING"])
+        try:
+            site = params.get("site")
+            content_id = params.get("content_id")
+            event = params.get("event")
+            path = params.get("path")
+            events_queue.put((site, content_id, event, path))
+        except Exception as e:
+            logger.error(str(e))
+
     else:
-        start_response('404 Not Found', [('Content-Type', 'text/plain')])
-        yield 'Nothing Here'
+        start_response("404 Not Found", [("Content-Type", "text/plain")])
+        yield "Nothing Here"
